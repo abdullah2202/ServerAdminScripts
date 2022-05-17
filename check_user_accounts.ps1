ce48c68ec3f060eb123002828446d148e42df18d
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-05-17 11:43:33 +0100

    Added Check User Accounts script
    Checks if users have account in Active Directory from a list of records in a csv.

---
+++ check_user_accounts.ps1
@@ -0,0 +1,47 @@
+﻿# Author: Mohammad Abdullah
+# Date: 17/05/2022
+# Description: Checks is a user has an AD account from a list of records in a csv.
+
+Import-Module activedirectory
+
+$filename = "users_check.csv"
+
+$records = @(Get-Content $filename).Length
+
+$Users = Import-csv $filename
+
+$exists = 0
+$doesnotexist = 0
+$count = 0
+
+
+foreach ($User in $Users) {
+
+   $count++
+
+   $pc = ($count/$records) * 100
+   $pc = [Math]::Round($pc, 0)
+
+   $Username = $User.SamAccountName
+
+   Write-Progress -Activity "Checking User: $Username" -Status "$pc% Complete" -PercentComplete $pc 
+
+    # If user already exists, change password to the one in the csv
+    if (Get-ADUser -F {SamAccountName -eq $Username}) {
+
+
+        Write-Host "User $Username exists." -ForegroundColor DarkYellow
+        $exists++
+    }
+    else {
+
+
+         Write-Host "User $Username does not exist." -ForegroundColor Cyan
+         $doesnotexist++
+    }
+
+}
+
+Write-Host "Exisiting accounts: $exists"
+Write-Host "Non Exisiting accounts: $doesnotexist"
+Write-Host "Total Accounts: $records"
