a96e3a0abb713fc1b5c2319e9fa4ac496d31e4ac
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-05-17 11:34:31 +0100

    Creation of Bulk Add/Edit Users in AD
    This script will iterate over a csv file and add/edit users in the AD.

---
+++ bulk_edit_add_users_in_ad.ps1
@@ -0,0 +1,63 @@
+﻿# Author: Mohammad Abdullah
+# Date: 17/05/2022
+# Description: Creates new users in AD, if they already exist, change their password to the one in the csv.
+
+Import-Module activedirectory
+
+$Users = Import-csv "G:\My Drive\2021-22\AD_PS\test.csv"
+
+$Group = "Students"
+
+
+$total_new = 0
+$total_changed = 0
+
+foreach ($User in $Users) {
+
+   $Username = $User.SamAccountName
+   $Password = ConvertTo-SecureString $User.password -AsPlainText -Force  
+
+    # If user already exists, change password to the one in the csv
+    if (Get-ADUser -F {SamAccountName -eq $Username}) {
+        
+        Set-ADAccountPassword -Identity $Username -NewPassword $Password -Reset
+
+        Write-Host "Password changed for $Username" -ForegroundColor DarkYellow
+        
+        $total_changed++
+    }
+    else {
+        # New User
+
+        $userProps = @{
+            SamAccountName             = $User.SamAccountName                   
+            Path                       = $User.path      
+            GivenName                  = $User.GivenName 
+            Surname                    = $User.Surname
+            Name                       = $User.Name
+            DisplayName                = $User.DisplayName
+            UserPrincipalName          = $user.UserPrincipalName
+            Description                = $User.Description
+            AccountPassword            = (ConvertTo-SecureString $User.password -AsPlainText -Force) 
+            Enabled                    = $true
+            ChangePasswordAtLogon      = $false
+            CannotChangePassword       = $true
+            HomeDrive                  = "N:"
+            HomeDirectory              = "\\hmount-csvr1\Users\$Username"
+            PasswordNeverExpires       = $true
+        }  
+
+         New-ADUser @userProps
+
+         #Add to group
+         Add-ADGroupMember -Identity $Group -Members $Username
+
+         Write-Host "The user account $Username is created." -ForegroundColor Cyan
+   
+         $total_new++
+    }
+
+}
+
+Write-Host "Total new accounts: $total_new"
+Write-Host "Total changed account: $total_changed"
