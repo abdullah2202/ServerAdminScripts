198e8bb4003a79d07136453e3680efa581b59152
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-07-13 12:51:22 +0100

    Added ffmpeg command in code
    script now calls the ffmpeg command, and then deletes all working files.

--- copy_and_rename_videos.py
+++ copy_and_rename_videos.py
@@ -1,6 +1,4 @@
-# Copies and renames files from iVMS, ready for FFMPEG to combine the videos into one
-
-# Use -> ffmpeg.exe -f concat -i file.txt -codec copy output.mp4 
+# Copies and renames files from iVMS, then runs ffmpeg and combines the files together and deletes working files.
 
 import shutil, os
 
@@ -13,21 +11,31 @@ def add_line(filename, text):
     with open(filename, "a+") as file_object:
         file_object.seek(0)
         data = file_object.read(100)
-        if len(data)>0: # Add new line if file contains data
+        if len(data)>0:
             file_object.write("\n")
         file_object.write(text)
 
 for term in terms:
     count = 0
     ff_file = newpath+term[:-1]+".txt"
+    single_files = []
     for file in os.listdir(cctvdir):    
         if term in file:
             count += 1
             filename = file.partition(".")
             newfilename = term+"_"+str(count)+"."+filename[2]
+            single_files.append(newfilename)
             shutil.copy(cctvdir+file,newpath+newfilename)
             add_line(ff_file,"file '"+newfilename+"'")
-            print("Renamed ",newfilename)
-        
+    
+    os.system("ffmpeg.exe -f concat -i {} -codec copy {}.mp4".format(ff_file,term[:-1]))
+    print("Created file: ", term[:-1],".mp4")
+    if count > 0:
+        os.remove(ff_file)
+        for file in single_files:
+            os.remove(newpath+file)
+
+
+
 
 
